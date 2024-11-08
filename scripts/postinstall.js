06167187a717a5c032a000e4f7943642a1ec00cc
Koushik Dutta <koushd@gmail.com>
2024-11-08 09:32:51 -0800

    update

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,5 +1,11 @@
 if (process.env.SKIP_INSTALL) {
-    console.log('skipping binary installation.');
+    console.log('skipping binary installation. SKIP_INSTALL is set.');
+    process.exit(0);
+}
+
+const fs = require('fs');
+if (process.env.SCRYPTED_FFMPEG_PATH && fs.existsSync(process.env.SCRYPTED_FFMPEG_PATH)) {
+    console.log('skipping binary installation. SCRYPTED_FFMPEG_PATH is set.');
     process.exit(0);
 }
 
